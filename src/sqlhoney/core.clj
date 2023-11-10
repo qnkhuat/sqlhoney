3b521cfdb5410120deaf48a2be5a22653c2193f8
Ngoc Khuat <qn.khuat@gmail.com>
2023-11-10 09:48:22 +0700

    onto expressions

--- src/sqlhoney/core.clj
+++ src/sqlhoney/core.clj
@@ -1,10 +1,15 @@
 (ns sqlhoney.core
-  (:refer-clojure :exclude [format])
+  (:refer-clojure :exclude [format #_defmethod])
   (:require
    [honey.sql :as hsql]
    [honey.sql.helpers :as hsql.helpers]
-   [methodical.core :as m])
+   #_[methodical.core :as m])
   (:import
+   (net.sf.jsqlparser.expression
+    DoubleValue
+    NullValue
+    LongValue
+    StringValue)
    (net.sf.jsqlparser.parser CCJSqlParserUtil)
    (net.sf.jsqlparser.schema
     Column
@@ -14,48 +19,86 @@
     SelectItem
     PlainSelect)))
 
-(m/defmulti jsql->honeysql
-  class)
+(defmulti jsql->honeysql class)
 
-(m/defmethod jsql->honeysql :default
-  [obj]
-  (throw (ex-info (clojure.core/format "No implementation for %s" (class obj)) {:obj obj})))
+#_(defmacro defmethod
+    [name dispatch-val method-args & body]
+    `(do
+      (clojure.core/defmethod ~name ~dispatch-val
+        [& args#]
+        (when debug (println "IN" args#))
+        (let [result# (apply (fn [~@method-args] ~@body) args#)]
+          (when debug (println "OUT" result#))
+          result#))))
 
-(m/defmethod jsql->honeysql PlainSelect
-  [^PlainSelect obj]
-  (merge
-   (when-let [selects (.getSelectItems obj)]
-     (apply hsql.helpers/select (map jsql->honeysql selects)))
-   (when-let [from (.getFromItem obj)]
-     (hsql.helpers/from #p (jsql->honeysql from)))))
 
-(m/defmethod jsql->honeysql SelectItem
-  [^SelectItem obj]
-  (let [expression (jsql->honeysql (.getExpression obj))]
-    (if-let [the-alias (.getAlias obj)]
-      [expression (.getName the-alias)]
-      expression)))
-
-(m/defmethod jsql->honeysql AllColumns
+(defmethod jsql->honeysql AllColumns
   [^AllColumns obj]
   (str obj))
 
-(m/defmethod jsql->honeysql Column
+(defn- maybe-alias
+  "Alias hsql-form if `obj` has an alias"
+  [hsql-form obj]
+  (if-let [the-alias (.getAlias obj)]
+    [hsql-form (.getName the-alias)]
+    hsql-form))
+
+(defmethod jsql->honeysql :default
+  [obj]
+  (throw (ex-info (clojure.core/format "No implementation for %s" (class obj)) {:obj obj})))
+
+;; net.sf.jsqlparser.statement.select
+(defmethod jsql->honeysql PlainSelect
+  [^PlainSelect obj]
+  (merge
+   (when-let [selects (.getSelectItems obj)]
+     (def selects selects)
+     (apply hsql.helpers/select (map jsql->honeysql selects)))
+   (when-let [from (.getFromItem obj)]
+     (hsql.helpers/from (jsql->honeysql from)))))
+
+(defmethod jsql->honeysql AllColumns
+  [^AllColumns obj]
+  (str obj))
+
+(defmethod jsql->honeysql SelectItem
+  [^SelectItem obj]
+  (maybe-alias (jsql->honeysql (.getExpression obj)) obj))
+
+;; net.sf.jsqlparser.expression
+
+(defmethod jsql->honeysql DoubleValue
+  [^DoubleValue obj]
+  (.getValue obj))
+
+(defmethod jsql->honeysql LongValue
+  [^LongValue obj]
+  (.getValue obj))
+
+(defmethod jsql->honeysql StringValue
+  [^StringValue obj]
+  (.getValue obj))
+
+;; net.sf.jsqlparser.schema
+(defmethod jsql->honeysql Column
   [^Column obj]
   (.getFullyQualifiedName obj true))
 
-(m/defmethod jsql->honeysql Table
+(defmethod jsql->honeysql Table
   [^Table obj]
-  (let [table-name (.getName obj)]
-    (if-let [the-alias (.getAlias obj)]
-      [table-name (.getName the-alias)]
-      table-name)))
+  (maybe-alias (.getName obj) obj))
 
 (defn format
   [query]
-  (#_methodical.util.trace/trace jsql->honeysql (CCJSqlParserUtil/parse query)))
+  (jsql->honeysql (CCJSqlParserUtil/parse query)))
 
-(try
- (format "select id as a from orders")
- (catch Exception e
-   e))
+
+(comment
+ (use 'clojure.tools.trace)
+ (clojure.tools.trace/trace-ns *ns*)
+ (remove-all-methods jsql->honeysql)
+
+ (try
+  (format "select 1 as a")
+  (catch Exception e
+    e)))
--- test/sqlhoney/core_test.clj
+++ test/sqlhoney/core_test.clj
@@ -1,12 +1,51 @@
 (ns sqlhoney.core-test
   (:require
-   [clojure.test :refer :all]
+   [honey.sql :as hsql]
+   [clojure.test :refer [deftest is testing]]
    [sqlhoney.core :as shoney]))
 
-(deftest simple-select-star-test
-  (is (= {:select ["*"] :from ["users"]}
-         (shoney/format "select * from users"))))
+(defmacro ^:private f
+  [query]
+  `(let [hsql-query# (shoney/format ~query)]
+     ;; test to see if we can convert from hsql to raw sql
+     (is (some? (hsql/format hsql-query#)))
+     hsql-query#))
 
-(deftest select-with-alias-test
-  (is (= {:select [["first_name" "fname"]] :from ["users"]}
-         (shoney/format "select first_name as fname from users"))))
+(deftest simple-select-test
+  (testing "select all"
+    (is (= {:select ["*"]
+            :from   ["users"]}
+           (f "select * from users")))
+
+    (testing "from with alias"
+      (is (= {:select ["*"]
+              :from   [["users" "u"]]}
+             (f "select * from users as u")))))
+
+  (testing "single select"
+    (is (= {:select ["id"]
+            :from   ["users"]}
+           (f "select id from users")))
+
+    (testing "with alias"
+      (is (= {:select [["first_name" "fname"]]
+              :from   ["users"]}
+             (f "select first_name as fname from users")))))
+
+  (testing "multiple select with alias"
+    (is (= {:select ["id" ["first_name" "fname"]]
+            :from   [["users" "u"]]}
+           (f "select id, first_name as fname from users as u")))))
+
+(deftest select-expression-test
+  (testing "simple"
+    (is (= {:select [1]}
+           (f "select 1")))
+    (is (= {:select [1.5]}
+           (f "select 1.5")))
+    (is (= {:select [1 2]}
+           (f "select 1, 2")))
+    (is (= {:select ["a"]}
+           (f "select 'a'")))
+    (is (= {:select [[1 "a"]]}
+           (f "select 1 as a")))))
